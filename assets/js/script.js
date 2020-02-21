247f72836780655a2d657276f11b9c7944c8f711
stacey bonareri <stacylorrain@gmail.com>
2020-02-21 17:43:37 +0300

    added hover

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $("#port").hover(function(){
-        $(this).find("description").show();
+    $("div.col-md-3#port").hover(function(){
+        $(this).find(".description").css("display", "flex");
     },function(){
-        $(this).find("description").hide();
+        $(this).find(".description").hide();
     });
 });
