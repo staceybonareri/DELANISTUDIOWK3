6f917c230f1ef4cc634cb7e0a004965fec149613
stacey bonareri <stacylorrain@gmail.com>
2020-02-24 12:11:07 +0300

    added alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@ $(document).ready(function(){
         $(this).find(".description").hide();
     });
 });
+
+$("#submit").click(function(){
+   var name = $("#btmcont div input[type=text][name=name]").val();
+   var email = $("#btmcont div input[type=email][name=email]").val();
+   var message = $("#btmcont div textarea[name=message]").val();
+   if(name && email && message !== null){
+       alert("Hello " + name +" we have received your message. Thank you for reaching out to us. ");
+   }else {
+       alert("You Must Fill All Fields!")
+   }
+});
