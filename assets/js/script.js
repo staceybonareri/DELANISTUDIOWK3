21d12cb8c960649b2400498af1b9a2a4433daa57
stacey bonareri <stacylorrain@gmail.com>
2020-02-25 09:52:52 +0300

    added effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,25 @@ $(document).ready(function(){
     $("div.col-md-3#port").hover(function(){
         $(this).find(".description").css("display", "flex");
     },function(){
-        $(this).find(".description").hide();
+        $(this).find(".description").fadeOut();
     });
-});
 
-$("#submit").click(function(){
-   var name = $("#btmcont div input[type=text][name=name]").val();
-   var email = $("#btmcont div input[type=email][name=email]").val();
-   var message = $("#btmcont div textarea[name=message]").val();
-   if(name && email && message !== null){
-       alert("Hello " + name +" we have received your message. Thank you for reaching out to us. ");
-   }else {
-       alert("You Must Fill All Fields!")
-   }
+    $("div#star div.row .col-sm").click(function(){
+        //alert("");
+        $(this).find(".visible").slideToggle();
+        $(this).find(".hidden").slideToggle();
+    })
+
+
+
+    $("#submit").click(function(){
+    var name = $("#btmcont div input[type=text][name=name]").val();
+    var email = $("#btmcont div input[type=email][name=email]").val();
+    var message = $("#btmcont div textarea[name=message]").val();
+    if(name && email && message !== null){
+        alert("Hello " + name +" we have received your message. Thank you for reaching out to us. ");
+    }else {
+        alert("You Must Fill All Fields!")
+    }
+    });
 });
